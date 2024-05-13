7497193a87ee2dc6053a71fb49eb8dde42fe012c
Rahul <rahultaakhr@gmail.com>
2024-05-13 23:05:11 +0530

    done

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const port = process.env.PORT
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/login', (req, res) => {
+  res.send('rahu_l_hr')
+})
+app.get('/SSS', (req, res) => {
+  res.send('Hello inn')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
